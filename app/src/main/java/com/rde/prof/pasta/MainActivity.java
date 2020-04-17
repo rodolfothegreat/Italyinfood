f15919b2a884d9fe5d7df75b0dcf0d6af684263c
Rodolfo d'Ettorre <rodolfothegreat@hotmail.com>
2020-04-17 16:12:52 +1000

    Initial commit

---
+++ app/src/main/java/com/rde/prof/pasta/MainActivity.java
@@ -0,0 +1,22 @@
+package com.rde.prof.pasta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide(); //<< this
+        setContentView(R.layout.activity_main);
+        WebView myWebView = (WebView) findViewById(R.id.webViewLorenzo);
+        myWebView.getSettings().setBuiltInZoomControls(true);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.getSettings().setDomStorageEnabled(true);
+        myWebView.loadUrl("file:///android_asset/lorenzo/Pasta.html");
+
+    }
+}
